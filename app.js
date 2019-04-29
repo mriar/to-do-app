2aad9dc3d6f8d80443cd36cafa826ea1533a00ea
manpreet <mriar@live.com>
2019-04-29 11:30:04 -0700

    assignment-5: update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function onReady() {
-  let todos = [];
-  let todoId = 1;
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -11,13 +10,6 @@ function onReady() {
      // get the text
     let title = newToDoText.value;
     console.log("title: ", title);
-
-    let todoobj = {
-      name: title,
-      id: todoId
-    }
-    todoId++;
-    todos.push(todoobj);
 
     // create a new li
     let newLi = document.createElement('li');
@@ -32,35 +24,34 @@ function onReady() {
     deleteBTN.textContent = "Delete";
 
     deleteBTN.addEventListener('click', function(event) {
-      let buttonLiText = this.parentElement.childNodes[0].textContent;
-      //this.parentElement represents the button's parent <li>
-      toDoList.removeChild(this.parentElement);
+        let buttonLiText = this.parentElement.childNodes[0].textContent;
+        //this.parentElement represents the button's parent <li>
+        toDoList.removeChild(this.parentElement);
 
-      todos.forEach(function(item) {
-        //console.log(item,index);
-        if(currentToDo === buttonLiText){
-        //remove from array
-        todos.splice(index, 1)
-      }
-      console.log(todos);
+        todos.forEach(function(item) {
+          //console.log(item,index);
+          if(currentToDo === buttonLiText){
+          //remove from array
+          todos.splice(index, 1)
+        }
+        console.log(todos);
 
+        });
       });
-       // set the title
-      newLi.textContent = title;
+         // set the title
+        newLi.textContent = title;
 
-      // attach the checkbox to the li
-      newLi.appendChild(checkbox);
+        // attach the checkbox to the li
+        newLi.appendChild(checkbox);
 
-      newli.appendchild(deleteBTN);
+        newLi.appendChild(deleteBTN);
+        // attach the li to the ul
+        toDoList.appendChild(newLi);
 
-      // attach the li to the ul
-      toDoList.appendChild(newLi);
-
-      //empty the input
-      newToDoText.value = '';
-    });
-  });
-};
+        //empty the input
+        newToDoText.value = '';
+      });
+  };
 
   window.onload = function() {
       onReady();
